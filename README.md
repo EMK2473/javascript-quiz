39b5753981dcc72510249532e72e803ca370d5c9
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-09-16 18:28:46 -0400

    developed storage functions

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # javascript-quiz
 Challenge 4: JavaScript Quiz
+Description: This JavaScript Quiz is designed to test your knowledge through 4 trivial questions.
 
+You will be given 10 seconds to answer the first question. If you get that question right, you will be given more time for the next question. If you get that question wrong, it will deduct 10 seconds from the timer. 
 
+If you run out of time, you will lose; and your score will be a reflected by the time left on the timer.
 
-
-
-
+Try to get a score of 100!
 
 
 GitHub Repo Link: https://github.com/EMK2473/javascript-quiz/
 
 Deployed Link: https://emk2473.github.io/javascript-quiz/
+
+
+![JavaScript Screenshot](./assets/JS%20Quiz%20Screenshot%20.jpg)
--- script.js
+++ script.js
@@ -35,16 +35,20 @@ const Questions = [
       { text: "FUNCTION:", isCorrect: false },
     ],
   },
-  "muahaha i apparently need this to no skip my array"
+  "muahaha i apparently need this to not skip q#1 in my array"
   // please give feedback on how I could fix this?
 ];
 let currentQuest = 0; 
 let currentScore = 0; 
-let secondsLeft = 60; 
+let secondsLeft = 10; 
 let startBut = document.querySelector("#startBtn");
+let nextBut = document.querySelector("#nextBut")
+
 function startQuestions() {
   startTimer();
   nextQuestion();
+  startBut.style.display = 'none';
+  nextBut.style.display = 'block';
 }
 startBut.addEventListener("click", function () {
   if ((currentQuest == 0)) {
@@ -55,7 +59,10 @@ function showScore() {
   let totalScore = document.getElementById("score");
   totalScore.textContent = `You scored ${score, + secondsLeft} points`;
   document.getElementById("timer").remove();
+  document.getElementById("tryAgainBtn").style.display = "block";
+  document.getElementById("highscoreBtn").style.display = "block";
 }
+
 function nextQuestion() {
   if (currentQuest < Questions.length - 1) {
     let question = document.getElementById("quest");
@@ -88,23 +95,95 @@ function checkAnswer() {
   );
   if (Questions[currentQuest - 1].a[chosenAnswer].isCorrect) {
     score++;
-    secondsLeft += 11;
+    secondsLeft += 24;
     nextQuestion();
   } else {
     secondsLeft -= 10;
     nextQuestion();
   }
-  // conditional to stop timer when no questions/showScore
 }
 function startTimer() {
   let timerInterval = setInterval(function () {
     secondsLeft--;
     document.querySelector("#timer").textContent = secondsLeft;
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0) {
+      document.getElementById("answer").remove();
+      document.getElementById("quest").remove();
+      document.getElementById("nextBut").remove();
       showScore();
       clearInterval(timerInterval);
-      sendMessage();
     }
   }, 1000);
 }
 
+function tryAgain(){
+  location.reload()
+}
+
+
+
+
+// need to implement how to store and render High Scores
+function viewHS(){
+  document.getElementById("highscoreBtn").style.display = "none";
+  document.getElementById("score").style.display = "none";
+  document.getElementById("hsLog").style.display = "block";
+}
+
+let hsInput = document.querySelector("#hsText");
+let hsForm = document.querySelector("#hsLog");
+let hsList = document.querySelector("#hsList");
+let hScores = []
+
+function renderHS() {
+  hsList.innerHTML = "";
+  for (let i = 0; i < hScores.length; i++) {
+    let hScore = hScore[i];
+    let li = document.createElement("li");
+    li.textContent = hScore;
+    li.setAttribute("data-index", i);
+    let button = document.createElement("button");
+    button.textContent = "Clear High Score";
+    li.appendChild(button);
+    hsList.appendChild(li);
+  }
+}
+function initStorage() {
+  let storedHighScores = JSON.parse(localStorage.getItem("hScores"));
+  if (storedHighScores !== null) {
+    hScores = storedHighScores;
+  }
+  renderHS();
+}
+
+function storeHS() {
+  localStorage.setItem("hScores", JSON.stringify(hScores));
+}
+
+// Add submit event to form
+hsForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let hsText = hsInput.value.trim();
+
+  if (hsText === "") {
+    return;
+  }
+  hScores.push(hsText);
+  hsInputInput.value = "";
+
+  storeHS();
+  renderHS();
+});
+
+hsList.addEventListener("click", function(event) {
+  let element = event.target;
+  if (element.matches("button") === true) {
+    let index = element.parentElement.getAttribute("data-index");
+    hScores.splice(index, 1);
+    storeHS();
+    renderHS();
+  }
+});
+
+initStorage()
